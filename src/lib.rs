7a065387bc4fcb653aebdc913389ff61cd4efacf
ishaksebsib <ishaksebsib@gmail.com>
2025-07-31 18:51:08 +0300

    feat: remove it works test from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
 
 
 pub mod prompt;
-
-#[cfg(test)]
-mod tests {
-    use crate::prompt::{Prompt, Promptable};
-
-    #[test]
-    fn it_works() {
-        let prompt = Prompt::new("test");
-        assert_eq!(prompt.name(), "test");
-    }
-}
